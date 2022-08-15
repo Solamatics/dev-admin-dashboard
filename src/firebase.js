12990eadf67912d2d6b28b7bf2e2354def208e42
Olusola Afikode <afikodeolaitan@gmail.com>
2022-08-15 14:10:41 +0100

    implement login with firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "dev-admin-98e9a.firebaseapp.com",
+  projectId: "dev-admin-98e9a",
+  storageBucket: "dev-admin-98e9a.appspot.com",
+  messagingSenderId: "825878335029",
+  appId: "1:825878335029:web:40582e409c26ac87b12b7c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
